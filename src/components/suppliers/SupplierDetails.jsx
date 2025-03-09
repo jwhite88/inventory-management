7e0d4d34435015a43b38719727130d4debc1fd19
jwhite88 <jwhite789@protonmail.com>
2025-03-09 16:57:08 -0400

    Added template code to the React components

---
+++ src/components/suppliers/SupplierDetails.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SupplierDetails() {
+  return (
+    <div>SupplierDetails</div>
+  )
+}
---
+++ src/components/suppliers/SupplierForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SupplierForm() {
+  return (
+    <div>SupplierForm</div>
+  )
+}
